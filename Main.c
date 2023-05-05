7e47d5970ca1e445eb4f4e9f6bedf328a3a4f2c1
Fazhirarizky <132472434+Fazhirarizky@users.noreply.github.com>
2023-05-05 22:48:20 +0700

    penambahan kode soal
    Menambahkan 5 soal

--- Main.c
+++ Main.c
@@ -1,7 +1,53 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include <time.h>
+#include <ctype.h>
 
+typedef struct {
+    char pertanyaan[500];
+    char jawaban[50];
+    int level;
+} pertanyaan;
+
+int main() {
+
+   int input[0];
+   while (1) {                                                                                                                                             
+
+    printf("\nTekan angka manapun untuk melanjutkan permainan, atau huruf 'n' untuk membatalkan dari permainan: \n");
+        scanf("%s", input);
+        if (isdigit(input[0])) { 
+        } else if (input[0] == 'n' || input[0] == 'N') { // menanyakan apakah pengguna yakin ingin keluar dari permainan
+            printf("\nApakah kamu yakin ingin keluar dari permainan? (Y/N): ");
+            scanf("%s", input);
+            if (input[0] == 'y' || input[0] == 'Y') {
+                return 0; // keluar dari permainan
+            }
+        } else {
+            return 0;
+        }
+
+    pertanyaan pertanyaans[15];
+    strcpy(pertanyaans[0].pertanyaan, "Siapa Pemenang piala dunia 2024?");
+    strcpy(pertanyaans[0].jawaban, "A");
+    pertanyaans[0].level = 1;
+
+    strcpy(pertanyaans[1].pertanyaan, "apa ibu kota dari india?");
+    strcpy(pertanyaans[1].jawaban, "B");
+    pertanyaans[1].level = 2;
+
+    strcpy(pertanyaans[2].pertanyaan, "Apa yang bisa dilihat sekali dalam satu menit, dua kali dalam satu momen, dan tidak terlihat dalam seribu tahun?");
+    strcpy(pertanyaans[2].jawaban, "D");
+    pertanyaans[2].level = 3;
+
+    strcpy(pertanyaans[3].pertanyaan, "Apa nama danau terbesar di dunia?");
+    strcpy(pertanyaans[3].jawaban, "C");
+    pertanyaans[3].level = 4;
+
+    strcpy(pertanyaans[4].pertanyaan, "Kota mana yang berjulik 'Crossroads of the pacific' dan 'The Big pineapple'?");
+    strcpy(pertanyaans[4].jawaban, "D");
+    pertanyaans[4].level = 5;
 typedef struct registration
 {
     char nama[20];
